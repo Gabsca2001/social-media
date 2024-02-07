857fbbce03eb63e04358b56cb8c07c5279fdcd7e
Gabriele Scamardo <scamardo.website@gmail.com>
2024-02-07 21:31:43 +0100

    register and login

---
+++ api/controllers/auth.js
@@ -0,0 +1,60 @@
+export const register = (req, res) => {
+    //TODO
+    //check if user exists
+    const q = 'SELECT * FROM users WHERE username = ?'
+
+    db.query(q, [req.body.username], (err, data) => {
+        if (err) {
+            res.status(500).json(err)
+        } else if (data.length > 0) {
+            res.status(409).json("User already exists!")
+        }
+
+        //create user
+        const salt = bcrypt.genSaltSync(10);
+        const hashedPassword = bcrypt.hashSync(req.body.password, salt);
+
+        const q = "INSERT INTO users (username, email, password, name) VALUE (?)"
+
+        const values = [[req.body.username, req.body.email, hashedPassword, req.body.name]]
+
+        db.query(q, [values], (err, data) => {
+            if (err) {
+                res.status(500).json(err)
+            } else {
+                res.status(200).json("User created!")
+            }
+        })
+
+    })
+    
+}
+export const login = (req, res) => {
+    //TODO
+    const q = 'SELECT * FROM users WHERE username = ?'
+
+    db.query(q, [req.body.username], (err, data) => {
+        if (err) {
+            res.status(500).json(err)
+        } else if (data.length === 0) {
+            res.status(404).json("User not found!")
+        }
+
+        //check password
+        const checkPassword = bcrypt.compareSync(req.body.password, data[0].password)
+
+        if (checkPassword) {
+            res.status(200).json("User logged in!")
+        } else {
+            res.status(400).json("Wrong password!")
+        }
+
+    }
+    )
+
+}
+
+export const logout = (req, res) => {
+    //TODO
+    
+}
--- api/index.js
+++ api/index.js
@@ -1,6 +1,22 @@
 import Express from "express";
 const app = Express();
 
+import authRoutes from "./routes/auth.js";
+import postRoutes from "./routes/post.js";
+import userRoutes from "./routes/user.js";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+
+
+//middlewares
+app.use(Express.json());
+app.use(cors());
+app.use(cookieParser());
+
+app.use("/api/users", userRoutes)
+app.use("/api/auth", authRoutes)
+app.use("/api/posts", postRoutes)
+
 app.listen(8800, () => {
     console.log("Server is running on port 8800");
 });
